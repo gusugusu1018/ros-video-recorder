d7a84df39a56d8f097b12c5b654703243c77db63
gusugusu <yuji.ogusu@aist.go.jp>
2019-11-15 18:01:43 +0900

    add sample service client

---
+++ scripts/sample_stop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from std_srvs.srv import Empty
+
+def record_start():
+    rospy.wait_for_service('video_recorder/stop')
+    try:
+        service = rospy.ServiceProxy('video_recorder/stop',Empty)
+        responce = service()
+    except rospy.ServiceException, e:
+        rospy.logerr("[ros-video-recorder] Service call failed")
+
+if __name__ == "__main__":
+    record_start()
+
